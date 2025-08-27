9f190980109690919c944bf4b1651d567c07ef50
Grayson Anderson <90288414+sparkyga@users.noreply.github.com>
2025-08-27 15:29:52 +0000

    test 2

--- src/org/lsoc/Main.java
+++ src/org/lsoc/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
         System.out.println("This is a sample Java project structure.");
+        for(int i = 1; i <= 5; i++) {
+            System.out.println("Count: " + i);
+        }
     }
 }
